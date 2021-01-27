98682587dd13d3bfbb30cd6b6807e5ce04b0f2b9
hill <hilluo@163.com>
2021-01-27 20:30:06 +0800

    add use content

--- src/main/java/com/hill/mall/entity/User.java
+++ src/main/java/com/hill/mall/entity/User.java
@@ -1,4 +1,66 @@
 package com.hill.mall.entity;
 
 public class User {
+    public User() {
+    }
+
+    public User(int id, String name, String account, String password, int age) {
+        this.id = id;
+        this.name = name;
+        this.account = account;
+        this.password = password;
+        this.age = age;
+    }
+
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    private String name;
+    private  String account;
+    private  String password;
+    private  int age;
+
+
+
+
+
 }
